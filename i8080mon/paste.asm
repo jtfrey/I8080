0eeb38e8905aff9397abc6b0d0c65a53aff218f8
Jeffrey Frey <freyguy77@gmail.com>
2026-07-26 12:02:59 -0400

    Add comments for clarity

--- i8080mon/paste.asm
+++ i8080mon/paste.asm
@@ -1,16 +1,16 @@
-TTY_OUT     EQU     0
+TTY_OUT     EQU     0                           ; The TTY i/o is attached to channel 0
 
             CPU     8080
             ORG     2000h
             
-            LXI     H, CSTR
-LOOP:       MOV     A, M
-            ORA     A
-            JZ      DONE
-            OUT     TTY_OUT
-            INX     H
-            JMP     LOOP
-DONE:       MVI     A, '\n'
-            OUT     TTY_OUT
-            RST     0
-CSTR:       DB      'Hello, world!', 00h
+            LXI     H, CSTR                     ; HL <= address of string to output
+LOOP:       MOV     A, M                        ; A <= next character from string
+            ORA     A                           ; A <= A | A, set flags
+            JZ      DONE                        ; A is zero, the NUL terminator
+            OUT     TTY_OUT                     ; Write the character to the TTY
+            INX     H                           ; HL++
+            JMP     LOOP                        ; Go process the next character
+DONE:       MVI     A, '\n'                     ; Write a newline…
+            OUT     TTY_OUT                     ; …to the TTY…
+            RST     0                           ; …then call reset vector 0 (reenter monitor)
+CSTR:       DB      'Hello, world!', 00h        ; NUL-terminated string to print to TTY
